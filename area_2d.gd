309c77990957f3fc81359af43faeebef6b2a6e3f
youssef1234-eng <lmhdilkabir@gmail.com>
2025-05-14 22:10:30 +0100

    chof kijak

--- area_2d.gd
+++ area_2d.gd
@@ -4,5 +4,12 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	
+	if body.is_in_group("player"):
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue.dialogue"))
 	pass
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		queue_free()
+	pass 
