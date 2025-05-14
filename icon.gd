228563c99ee0eb315736d88a29b2544600f30256
youssef1234-eng <lmhdilkabir@gmail.com>
2025-05-14 21:02:02 +0100

    bdel thakom

---
+++ icon.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+var speed = 300
+
+
+func _physics_process(delta: float) -> void:
+	
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = - speed
+	
+	elif Input.is_action_pressed("right"):
+		velocity.x = speed
+	
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_pressed("up"):
+		velocity.y = - speed
+	
+	elif Input.is_action_pressed("down"):
+		velocity.y = speed
+	
+	else:
+		velocity.y = 0
+	
+	
+	
+	
+	
+	
+	
+	move_and_slide()
