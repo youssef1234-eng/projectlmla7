c51e7f60f1df29eca8d71a29cf2f79611d207a99
youssef1234-eng <lmhdilkabir@gmail.com>
2025-05-14 23:31:40 +0100

    assets
    tilmap tina enemys ana

--- scripts/area_2d.gd
+++ scripts/area_2d.gd
@@ -1,8 +1,6 @@
 extends Area2D
 
 
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		DialogueManager.show_example_dialogue_balloon(load("res://dialogue.dialogue"))
