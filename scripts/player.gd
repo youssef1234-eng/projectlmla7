786df631d80d8c23cc6a71a8e51c4bfc06693c2f
Tizni-aha <medkhoul8@gmail.com>
2025-05-16 01:13:13 +0100

    hwatna lhayat

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,47 +1,53 @@
 extends CharacterBody2D
 
-
-var speed = 250
-var jump = 350
-var gravity = 15
-
-@onready var animation: AnimatedSprite2D = $Sprite2D
+var speed = 150
+var jumb = 250
+var gravity = 14
 
 
 
-func _physics_process(delta: float) -> void:
+@onready var sprite_2d = $Sprite2D
+
+
+
+
+func _physics_process(delta):
 	
-	if (velocity.x > 1 and velocity.x > -1):
-		animation.animation = "runing"
+	Move()
+	Jumb()
+	Gravity()
+	
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
 	else:
-		animation.animation = "idle"
+		sprite_2d.animation = "default"
+		
 	move_and_slide()
 	
 	var isleft = velocity.x < 0
-	animation.flip_h = isleft
+	sprite_2d.flip_h = isleft
 	pass
 	
 	
+func Move():
 	if Input.is_action_pressed("left"):
-		velocity.x = - speed
+		velocity.x = -speed
 	elif Input.is_action_pressed("right"):
 		velocity.x = speed
 	else:
-		velocity.x = 0
-
+		velocity.x = move_toward(velocity.x, 0,10)
+	pass
 	
-	if not is_on_floor(): 
+	
+func Jumb():
+	if Input.is_action_just_pressed("jumb") and is_on_floor():
+		velocity.y = -jumb
+		
+	pass
+	
+	
+func Gravity():
+	if not is_on_floor():
 		velocity.y += gravity
-		animation.animation = "jump"
-		
-	
-	if is_on_floor():
-		if Input.is_action_just_pressed("up"):
-			velocity.y = -jump
-			
-		
-	
-	
-	
-	
-	
+		sprite_2d.animation = "jumping"
+	pass
