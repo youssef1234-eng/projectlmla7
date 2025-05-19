a11b5d35c6de0c11f0d6610507204a9bfa925dba
youssef1234-eng <lmhdilkabir@gmail.com>
2025-05-19 18:09:13 +0100

    ldfmg
    https://github.com/youssef1234-eng/ait_saleh-to-anzi.git

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,43 +5,46 @@ var speed = 250
 var jump = 350
 var gravity = 15
 
-@onready var animation: AnimatedSprite2D = $Sprite2D
+@onready var animated: AnimatedSprite2D = $AnimatedSprite2D
 
 
 
 func _physics_process(delta: float) -> void:
 	
-	if (velocity.x > 1 and velocity.x > -1):
-		animation.animation = "runing"
-	else:
-		animation.animation = "idle"
-	move_and_slide()
 	
-	var isleft = velocity.x < 0
-	animation.flip_h = isleft
-	pass
-	
-	
-	if Input.is_action_pressed("left"):
-		velocity.x = - speed
-	elif Input.is_action_pressed("right"):
-		velocity.x = speed
-	else:
-		velocity.x = 0
-
 	
 	if not is_on_floor(): 
 		velocity.y += gravity
-		animation.animation = "jump"
+		animated.play("jump")
+
 		
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("up"):
 			velocity.y = -jump
 			
+	
+	
+	
+	
+	
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = - speed
+		animated.play("run")
+	elif Input.is_action_pressed("right"):
+		velocity.x = speed
+		animated.play("run")
+	else:
+		velocity.x = 0
+		animated.play("idle")
+
+	
+	
+			
 		
 	
-	
+	move_and_slide()
 	
 	
 	
